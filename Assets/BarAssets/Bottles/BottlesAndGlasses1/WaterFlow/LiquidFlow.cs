4bab537b3313d4da16a72c7499dc8032d69bfd69
alexzbaz <alexander.bazhanov@web.de>
2025-07-25 10:09:18 +0200

    Removed some code, generated new lightmap and added some content to Readme

--- Assets/BarAssets/Bottles/BottlesAndGlasses1/WaterFlow/LiquidFlow.cs
+++ Assets/BarAssets/Bottles/BottlesAndGlasses1/WaterFlow/LiquidFlow.cs
@@ -11,9 +11,8 @@ public class LiquidFlow : MonoBehaviour
     [SerializeField] private GameObject liquidInGlass;
     [SerializeField] private GameObject foam;
     [SerializeField] private AudioSource audiosource;
-    [SerializeField] private bool isTilting = false;
-    [SerializeField] private bool isNotTilting = false;
-
+    
+    private bool isTilting = false;
     private float tiltThreshold = -45f;
     private float tiltThreshold2 = 45f;
     private Material liquidInBottleMaterial;
@@ -21,18 +20,15 @@ public class LiquidFlow : MonoBehaviour
     private Material foamMaterial;
 
     private float bottleFillCurrent = 0.9f;
-    private float bottleFillEnd = 0.2f;
+    private float bottleFillEnd = 0.4f;
     private float glassFillCurrent = 0f;
     private float glassFillEnd = 0.85f; 
-    private float fillingSpeed = 0.1f;
-    //private bool isTilting = false;
-    //private bool isNotTilting = false;
+    private float fillingSpeed = 0.05f;
 
     // Start is called before the first frame update
     void Start()
     {
 		liquidFlow_ObiSolver.SetActive(false);
-        //audiosource.Play();
         GetAllMaterials();
         SetDefaultFillValues();
         SetColorOfLiquidInGlass();
@@ -47,17 +43,15 @@ public class LiquidFlow : MonoBehaviour
         if (tiltAngle < tiltThreshold || tiltAngle > tiltThreshold2)
         {
             // Trigger audiosource.Play() only once, when bottle is tilted for the first time
-            isNotTilting = false;
-
             if (isTilting == false) 
             {
-                Debug.Log("Bottle is tilting");
                 isTilting = true;
-                audiosource.Play(); 
+                audiosource.Play();
             }
 
             // Setup Water Emitter to the right direction
             Vector3 horizontalTilt = new Vector3(transform.up.x, 0f, transform.up.z);
+
             // Making sure that there is no division or multiplication by 0
             if (horizontalTilt.sqrMagnitude > 0.001f)
             {
@@ -66,9 +60,8 @@ public class LiquidFlow : MonoBehaviour
             }
 
 			liquidFlow_ObiSolver.SetActive(true);
-            //audiosource.Play();
 
-            // Change level of "Fill" in WhiskeyBottle
+            // Change level of "Fill" in Bottles
             if (liquidInBottleMaterial.HasProperty("_Fill"))
             {
                 ChangeBottleFill();                
@@ -83,10 +76,8 @@ public class LiquidFlow : MonoBehaviour
         {
             // Trigger audiosource.Stop() only once, when bottle is not tilted anymore for the first time
             isTilting = false;
-            if (isNotTilting == false) 
+            if (isTilting == false)
             {
-                Debug.Log("Bottle is not tilting");
-                isNotTilting = true;
                 audiosource.Stop();
             }
 
@@ -139,6 +130,7 @@ public class LiquidFlow : MonoBehaviour
         return angle;
     }
 
+    // Change the level of shader in the bottle
     void ChangeBottleFill()
 	{
         if (bottleFillCurrent > bottleFillEnd)
@@ -148,7 +140,8 @@ public class LiquidFlow : MonoBehaviour
         }
     }
 
-    void ChangeGlassFill()
+	// Change the level of shader in the glass
+	void ChangeGlassFill()
 	{
         if (glassFillCurrent < glassFillEnd)
         {
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # ZI_Barlab
+
+This repository contains the development of a virtual reality environment, called "The Barlab".
+
+# Overview
+
+This project consists of three parts:
+
+1. Environment (the barlab)
+2. Bottles and Liquids
+3. Interactions in VR
+
+## Environment
+
+The environment is built as a 3D-Unity-project. It consists of normal 3D-objects that are either custom-made or downloaded for free from the Unity Asset Store.
+
+## Bottles and Liquids
+
+As this project is supposed to be used in exposure-therapy, it is important that the bottles and liquids look realistic. Thus, the bottles are custom-made to look similar to real bottles.
+The liquids are split into 3 parts: Liquids in Bottles, Pouring Liquids into Glasses and Liquids in Glasses. The liquid-simulation in the bottles and glasses is done by using custom Shader Graphs, while the pouring simulation is implemented with ObiFluid.
+
+## Interactions
+
+The prototype uses hand-tracking as a default mode. No controllers are required to use the prototype. Bottles and Glasses are grabbable with custom hand gestures.
+
+# Dependencies
+
+This project has been developed with the Meta Quest 3.
+
+The operability with other HMDs has not been tested, except for the Quest 2.
+
+# Changing Bottles
+
+Changing the bottles and glasses is done in the editor of Unity. There, simply deactivate bottles and glasses that you want to remove and activate those that are required. This project has been designed with the requirement that only one bottle and one glass at a time will be tested.
+
+Bottles do not to need to be moved to a specific place as on start they will snap in front of the player.
